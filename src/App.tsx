c7bc83b3fe2ed33f9470ed0c0b782b35d0e9db77
Haya Tahboub <haya1tahboub@gmail.com>
2025-09-30 11:11:57 +0300

    feat(translation): add translation files and dependancies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import Router from './router/Router';
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 
 const App = () => {
+  const { i18n } = useTranslation();
+  useEffect(() => {
+    document.documentElement.setAttribute('dir', i18n.language === 'ar' ? 'rtl' : 'ltr');
+  }, [i18n.language]);
+
   return <RouterProvider router={Router} />;
 };
 
