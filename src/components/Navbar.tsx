628d90f8cad57a9af52cfa501c47c42cd6baaf1c
Mohammed Tamimi <mohammedtamimi72@gmail.com>
2025-10-02 13:05:59 +0300

    feat(Themes): applied dark theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 import { Button } from '@/components/ui/button/button';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { changeLanguage } from '@/localization/changeLang';
 import Logo from './Logo';
+import { useLanguage } from '@/hooks/useLanguage';
+import { useThemes } from '@/hooks/useThemes';
 
 const Navbar = () => {
-  const { t, i18n } = useTranslation();
-  const toggleLanguage = () => {
-    changeLanguage(i18n.language === 'en' ? 'ar' : 'en');
-  };
+  const { t } = useTranslation();
+  const { toggleLanguage } = useLanguage();
+  const { toggleTheme } = useThemes();
 
   const navigate = useNavigate();
   return (
@@ -37,6 +37,7 @@ const Navbar = () => {
         <Button onClick={() => navigate('signup')}>Sign Up</Button>
       </div>
       <Button onClick={toggleLanguage}>change language</Button>
+      <Button onClick={toggleTheme}>change theme</Button>
     </div>
   );
 };
---
+++ src/hooks/useThemes.ts
@@ -0,0 +1,10 @@
+import { useTheme } from '@/themes/useThemes';
+
+export const useThemes = () => {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+  return { toggleTheme };
+};
