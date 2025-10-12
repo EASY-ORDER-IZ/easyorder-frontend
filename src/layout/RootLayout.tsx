529cc7c5ca9913560866f3fd1a60d930558d4352
Mohammed Tamimi <mohammedtamimi72@gmail.com>
2025-10-12 11:15:02 +0300

    fix(input): comment the component in rootlayout page

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,22 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/CommonComponents/Navbar';
 import SignupBar from '../components/SignupBar';
-import InputComponent from '@/components/CommonComponents/Input';
-import { EyeIcon, LockKeyhole } from 'lucide-react';
+// import InputComponent from '@/components/CommonComponents/Input';
+// import { EyeIcon, LockKeyhole } from 'lucide-react';
 
 const RootLayout = () => {
   return (
     <div className="p-10">
       <SignupBar />
       <Navbar />
-      <div className="placeholder:text-text-200 placeholder:text-list mt-100 flex w-[600px] justify-center p-10 placeholder:leading-[100%] placeholder:font-medium">
+      {/* <div className="placeholder:text-text-200 placeholder:text-list mt-100 flex w-[600px] justify-center p-10 placeholder:leading-[100%] placeholder:font-medium">
         <InputComponent
           leadingIcon={<LockKeyhole className="text-text-200 text-h4" />}
           trailingIcon={<EyeIcon className="text-text-200 text-h4 cursor-pointer" />}
           type="password"
           placeholder="Password"
         />
-      </div>
+      </div> */}
 
       <Outlet />
     </div>
