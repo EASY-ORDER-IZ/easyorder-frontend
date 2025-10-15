61f2f4c9d813c25aa1e16698ea34837113e75921
Mohammed Tamimi <mohammedtamimi72@gmail.com>
2025-10-15 10:48:48 +0300

    fix(drawer): added drawer styles and remove dialog

--- src/components/FilterComponents/Drawer.tsx
+++ src/components/FilterComponents/Drawer.tsx
@@ -1,22 +1,22 @@
 'use client';
-import { Drawer, DrawerContent, DrawerTrigger, DrawerOverlay } from '@/components/ui/drawer';
+
+import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
 import { SlidersVertical } from 'lucide-react';
 import FilterComponent from './Filter';
 
 export default function DrawerComponent() {
   return (
-    <Drawer>
+    <Drawer shouldScaleBackground={true}>
       <DrawerTrigger asChild>
-        <div className="bg-disabled-bg rounded-7xl cursor-pointer px-3 py-3 sm:hidden">
+        <div className="bg-disabled-bg rounded-7xl flex cursor-pointer px-3 py-3 sm:hidden">
           <SlidersVertical className="h-5 w-5" />
         </div>
       </DrawerTrigger>
 
-      {/* Add overlay for background dimming */}
-      <DrawerOverlay className="fixed inset-0" />
-
-      <DrawerContent className="bg-background-main fixed bottom-0 left-1/2 z-50 flex w-full max-w-md -translate-x-1/2 items-center rounded-t-2xl p-6 shadow-lg">
-        <FilterComponent />
+      <DrawerContent className="flex h-[90vh] flex-col items-center justify-center !border-0 !bg-transparent">
+        <div className="bg-background-main scrollbar-thin scrollbar-thumb-gray-50 scrollbar-track-transparen rounded-t-1xl flex h-[95vh] justify-center overflow-y-auto">
+          <FilterComponent />
+        </div>
       </DrawerContent>
     </Drawer>
   );
