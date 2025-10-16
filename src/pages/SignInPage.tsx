dc7ca0e69cf69e34ea469bab2e38912ce9bed939
Mohammed Tamimi <mohammedtamimi72@gmail.com>
2025-10-16 11:57:16 +0300

    feat(checkbox): added checkbox and its styles

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,9 +1,8 @@
-import { Switch } from '@/components/ui/switch';
-
+import { Checkbox } from '@/components/ui/checkbox';
 const SignInPage = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-5 p-10">
-      <Switch className="" />
+      <Checkbox />{' '}
     </div>
   );
 };
