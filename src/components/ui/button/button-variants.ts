888c60ca55009dbe3101438acdad6d644bbc7b25
Mohammed Tamimi <mohammedtamimi72@gmail.com>
2025-10-19 11:56:33 +0300

    feat(button): added hover and disabled styles for button

--- src/components/ui/button/button-variants.ts
+++ src/components/ui/button/button-variants.ts
@@ -1,13 +1,14 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-  ' rounded-lg px-9 py-3.5 cursor-pointer flex items-center justify-center gap-2.5  ',
+  ' rounded-lg px-9 py-3.5 cursor-pointer flex  hover:bg-accent-hover  items-center justify-center gap-2.5  ',
   {
     variants: {
       variant: {
         default: '',
-        primary: 'bg-accent-primary text-background',
-        secondary: 'border border-accent-primary text-accent-primary',
+        primary: 'bg-accent-primary text-background disabled:bg-text-disabled  ',
+        secondary:
+          'border border-accent-primary text-accent-primary hover:text-background hover:border-0 disabled:text-text-disabled disabled:border-text-disabled ',
       },
       size: {
         default: '',
