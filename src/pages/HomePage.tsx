dcce6140110e65c0c93684862731f4edac3e58ce
Mohammed Tamimi <mohammedtamimi72@gmail.com>
2025-10-28 13:37:03 +0200

    fix: but the carousel inside the rate component and remove the code rom the footer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import Body from '@/components/HomePageComponents/Body';
-import Footer from '@/components/HomePageComponents/Footer';
+import Rate from '@/components/HomePageComponents/Rate';
 import Header from '@/components/HomePageComponents/Header';
+import Footer from '@/components/HomePageComponents/Footer';
 
 const HomePage = () => {
   return (
     <div className="flex w-full flex-col justify-center gap-10 pb-4">
       <Header />
       <Body />
+      <Rate />
       <Footer />
     </div>
   );
