3611a8a9ee549db779ed28be922fa1879935b323
tamimi-mayar <Mayar@iztechvalley.ps>
2025-12-01 18:52:03 +0200

    chore(dashboard): commit befre switch

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
 import Body from '@/components/HomePageComponents/Body';
-import Rate from '@/components/HomePageComponents/Rate';
-import Header from '@/components/HomePageComponents/Header';
-import Footer from '@/components/HomePageComponents/Footer';
+// import Rate from '@/components/HomePageComponents/Rate';
+// import Header from '@/components/HomePageComponents/Header';
+// import Footer from '@/components/HomePageComponents/Footer';
 
 const HomePage = () => {
   return (
     <div className="flex w-full flex-col justify-center gap-10 pb-4">
-      <Header />
+      {/* <Header /> */}
       <Body />
-      <Rate />
-      <Footer />
+      {/* <Rate /> */}
+      {/* <Footer /> */}
     </div>
   );
 };
