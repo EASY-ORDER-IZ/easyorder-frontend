f230d471ce90cf79cdc893d1131efdb948908c2c
tamimi-mayar <Mayar@iztechvalley.ps>
2025-12-04 13:48:38 +0200

    fix(footer): make it responsive

--- src/components/HomePageComponents/footer/Footer.tsx
+++ src/components/HomePageComponents/footer/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import LogoSVG from '@/assets/svg/logo';
 
 type FooterBackgroundProps = React.HTMLAttributes<HTMLElement> & {
@@ -11,16 +10,17 @@ type FooterBackgroundProps = React.HTMLAttributes<HTMLElement> & {
 const Footer: React.FC<FooterBackgroundProps> = ({ src, alt = '', className = '' }) => {
   return (
     <footer className={`relative w-full overflow-hidden ${className}`}>
-      <img src={src} alt={alt} className="absolute inset-0 h-full w-full object-cover" />
+      <img
+        src={src}
+        alt={alt}
+        className="absolute inset-0 h-[80px] w-[400px] sm:h-full sm:w-full sm:object-cover md:h-full"
+      />
 
       <div className="relative flex h-full w-full items-end justify-center px-4">
-        <div className="flex w-full max-w-[1229px] flex-col items-center gap-1 pb-2 sm:gap-2 sm:pb-4 md:gap-3 md:pb-6">
-          <LogoSVG
-            className="h-[0.78125rem] w-[5.4125rem] sm:h-[1.6625rem] sm:w-[11.444rem] md:h-[24rem] md:w-[20rem]"
-            color="white"
-          />
+        <div className="flex w-full max-w-[1129px] flex-col items-center gap-3 pb-6 lg:flex-row lg:items-center lg:justify-between lg:gap-6">
+          <LogoSVG color="white" className="" />
 
-          <span className="text-[10px] text-white sm:text-xs md:text-sm">
+          <span className="text-[10px] text-white sm:text-xs md:text-sm lg:text-sm">
             FirstChance © 2025, All Rights Reserved to IztechValley
           </span>
         </div>
@@ -28,4 +28,5 @@ const Footer: React.FC<FooterBackgroundProps> = ({ src, alt = '', className = ''
     </footer>
   );
 };
+
 export default Footer;
